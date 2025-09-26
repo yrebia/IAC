0466cac40789fbb3b5f21d9a5c7afc72caa17435
Pauline Tavernier <pauline.tavernier@epitech.eu>
2025-09-26 11:23:20 +0200

    multi env

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_vpc" "main" {
 
 resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = var.subnet_cidr
   tags = {
     Name = "${var.vpc_name}-subnet"
   }
