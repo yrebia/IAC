d3410191c34faf07ad3627e1f798a965efac9ea2
Mathieu MARQUES <mathieu1.marques@epitech.eu>
2025-09-26 16:44:22 +0200

    style: fix main.tf style

--- permissions/main.tf
+++ permissions/main.tf
@@ -40,34 +40,34 @@ resource "aws_iam_access_key" "jeremie" {
 }
 
 resource "aws_iam_openid_connect_provider" "github" {
-  url = "https://token.actions.githubusercontent.com"
-  client_id_list = ["sts.amazonaws.com"]
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
 }
 
 resource "aws_iam_role" "github_actions_role" {
-    name = "GitHubActionsRole"
-    assume_role_policy = jsonencode({
-        Version = "2012-10-17"
-        Statement = [
-        {
-            Effect = "Allow"
-            Principal = {
-            Federated = aws_iam_openid_connect_provider.github.arn
-            }
-            Action = "sts:AssumeRoleWithWebIdentity"
-            Condition = {
-            StringLike = {
-                "token.actions.githubusercontent.com:sub" = "repo:yrebia/IAC:*"
-            }
-            }
+  name = "GitHubActionsRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
         }
-        ]
-    })
-    tags = {
-        Project = var.project_id
-        Env     = var.env
-    }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:yrebia/IAC:*"
+          }
+        }
+      }
+    ]
+  })
+  tags = {
+    Project = var.project_id
+    Env     = var.env
+  }
 }
 
 resource "aws_iam_role_policy" "terraform_policy" {
