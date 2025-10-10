ec105acf1bc3cd2f70fec05d5ecccbc74a917409
Pauline Tavernier <pauline.tavernier@epitech.eu>
2025-10-10 11:50:27 +0200

    fix syntax

--- main.tf
+++ main.tf
@@ -1,9 +1,7 @@
-# --- K8s Helm release ---
+# --- Helm release (namespace app déjà créé hors TF) ---
 resource "helm_release" "task_manager" {
   name             = "task-manager"
-
-  namespace        = kubernetes_namespace.app.metadata[0].name
-
+  namespace        = "app"
   chart            = "${path.module}/charts/task-manager"
   create_namespace = false
 
@@ -21,15 +19,21 @@ resource "helm_release" "task_manager" {
 # --- Réseau (VPC + Subnet) ---
 resource "aws_vpc" "main" {
   cidr_block = var.cidr_block
-  tags = { Name = var.vpc_name }
+  tags       = { Name = var.vpc_name }
 }
 
 resource "aws_subnet" "main" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.subnet_cidr
   availability_zone = var.subnet_az
-  tags = { Name = "${var.vpc_name}-subnet" }
+  tags              = { Name = "${var.vpc_name}-subnet" }
 }
 
-output "vpc_id"    { value = aws_vpc.main.id }
-output "subnet_id" { value = aws_subnet.main.id }
+# --- Outputs ---
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "subnet_id" {
+  value = aws_subnet.main.id
+}
