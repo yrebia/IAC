1efa49d09a3d65f76e6c2a96953e4028a1080a68
Pauline Tavernier <pauline.tavernier@epitech.eu>
2025-10-10 11:46:37 +0200

    refactor(ci): manage K8s namespace via Terraform and remove kubectl step

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
 # --- K8s Helm release ---
 resource "helm_release" "task_manager" {
   name             = "task-manager"
-  namespace        = "app"
+
+  namespace        = kubernetes_namespace.app.metadata[0].name
+
   chart            = "${path.module}/charts/task-manager"
   create_namespace = false
 
@@ -19,27 +21,15 @@ resource "helm_release" "task_manager" {
 # --- Réseau (VPC + Subnet) ---
 resource "aws_vpc" "main" {
   cidr_block = var.cidr_block
-
-  tags = {
-    Name = var.vpc_name
-  }
+  tags = { Name = var.vpc_name }
 }
 
 resource "aws_subnet" "main" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = var.subnet_cidr
   availability_zone = var.subnet_az
-
-  tags = {
-    Name = "${var.vpc_name}-subnet"
-  }
+  tags = { Name = "${var.vpc_name}-subnet" }
 }
 
-# --- Outputs ---
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
-
-output "subnet_id" {
-  value = aws_subnet.main.id
-}
+output "vpc_id"    { value = aws_vpc.main.id }
+output "subnet_id" { value = aws_subnet.main.id }
