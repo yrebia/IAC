c76bff509742b87ae7353b68772ec84f3b20aacc
AldricJourdain <aldric.jourdain@epitech.eu>
2025-10-10 16:21:19 +0200

    refactor: moved outputs to outputs.tf

--- main.tf
+++ main.tf
@@ -39,26 +39,6 @@ resource "aws_subnet" "db" {
   }
 }
 
-# --- Outputs ---
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
-
-output "subnet_id" {
-  value = aws_subnet.main.id
-}
-
-output "db_endpoint" {
-  value       = aws_db_instance.main.endpoint
-  description = "RDS instance endpoint"
-}
-
-output "db_secret_arn" {
-  value       = length(aws_db_instance.main.master_user_secret) > 0 ? aws_db_instance.main.master_user_secret[0].secret_arn : null
-  description = "ARN du secret contenant les credentials de la base de données"
-  sensitive   = true
-}
-
 resource "aws_db_subnet_group" "main" {
   name       = "${var.project_id}-db-subnet-group"
   subnet_ids = [aws_subnet.main.id, aws_subnet.db.id]
