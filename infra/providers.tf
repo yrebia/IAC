6b71fa502c1b409410b8f5a360b9e30d6070458d
Pauline Tavernier <pauline.tavernier@epitech.eu>
2025-10-21 19:48:50 +0200

    fix

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,10 +1,9 @@
 data "aws_eks_cluster" "this" {
-  name       = var.cluster_name
-  depends_on = [module.eks]
+  name = module.eks.cluster_name
 }
 
 data "aws_eks_cluster_auth" "this" {
-  name = var.cluster_name
+  name = module.eks.cluster_name
 }
 
 provider "kubernetes" {
